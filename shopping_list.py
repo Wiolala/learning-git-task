64953b0e27c6e5f7b0e3ee6c54eceb92ab84e5f5
Wioleta Zelek <wioleta.zelek@gmail.com>
2020-08-11 17:07:08 +0000

    Print number of products

--- shopping_list.py
+++ shopping_list.py
@@ -4,3 +4,6 @@ shopping_list = {
 }
 for shop, things in shopping_list.items():
     print("Idę do: " + shop.capitalize() + ", kupuję tu: " + str(things)) 
+
+number_of_products = (len(shopping_list["piekarnia"]) + len(shopping_list["warzywniak"]))
+print("W sumie kupuję " +str(number_of_products) + " produktów.")
