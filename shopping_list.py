684b13ecbcb8c96e4cfb2fa63a7f25990669f9f9
Wioleta Zelek <wioleta.zelek@gmail.com>
2020-08-11 16:58:35 +0000

    iterate dictionary

--- shopping_list.py
+++ shopping_list.py
@@ -2,3 +2,5 @@ shopping_list = {
     "piekarnia" : ["chleb", "bułki", "pączki"],
     "warzywniak" : ["marchew", "seler", "rukola"]
 }
+for shop, things in shopping_list.items():
+    print("Idę do: " + shop.capitalize() + ", kupuję tu: " + str(things)) 
